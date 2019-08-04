74497d25bdeb72c8d5480c240a11db7eb08e6b55
Sheng <nstop.sheng@gmail.com>
2019-08-05 06:41:36 +0800

    change to chacha20

--- startup.sh
+++ startup.sh
@@ -4,10 +4,10 @@
 set -m
 
 # Start the primary process and put it in the background
-ss-manager -m chacha20-ietf-poly1305 -u -d 1.1.1.1 --plugin obfs-server --plugin-opts "obfs=http" --manager-address 127.0.0.1:6001 &
+ss-manager -m chacha20 -u -d 1.1.1.1 --plugin obfs-server --plugin-opts "obfs=http" --manager-address 127.0.0.1:4001 &
 
 # Start the helper process
-ssmgr -c default.yml
+ssmgr -c default.yml -m 0.0.0.0:4002
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
